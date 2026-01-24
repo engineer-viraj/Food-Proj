0ff9af05b2c0ff34bbba7275ec5c38669826345e
Viraj kumar <virajk748@gmail.com>
2026-01-24 23:20:47 +0530

    Omni-Food-service

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,24 +1,35 @@
-$(document).ready(function() {
-  /* For the sticky navigation */
-  // ... (Your sticky navigation code can go here)
+$(document).ready(function () {
 
-  /* Smooth scrolling for navigation */
-  // Use event delegation for better performance
-  $('body').on('click', 'a[href*="#"]:not([href="#"])', function(event) {
-    // Check if the link is on the current page
+  /* Sticky navigation */
+  $('.js--section-features').waypoint(function (direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px'
+  });
+
+  /* Scroll on buttons */
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+  });
+
+  /* Navigation scroll */
+  $('a[href*="#"]:not([href="#"])').click(function (event) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       const hash = this.hash;
 
-      // Animate scroll
-      if ($(hash).length) { // Make sure the target element exists
+      if ($(hash).length) {
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 800, function() {
-          // Add hash to URL after scrolling
+        }, 800, function () {
           window.location.hash = hash;
         });
       }
@@ -26,16 +37,30 @@ $(document).ready(function() {
   });
 
   /* Animations on scroll */
-  $('.js--section-features').waypoint(function(direction) {
+  $('.js--wp-1').waypoint(function (direction) {
     $('.js--wp-1').addClass('animate__animated animate__fadeIn');
   }, {
     offset: '50%'
   });
 
-  $('.js--section-steps').waypoint(function(direction) {
+  $('.js--wp-2').waypoint(function (direction) {
     $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
   }, {
     offset: '50%'
+  });
+
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon ion-icon');
+
+    nav.slideToggle(200);
+
+    if (icon.attr('name') === 'menu-outline') {
+      icon.attr('name', 'close-outline');
+    } else {
+      icon.attr('name', 'menu-outline');
+    }
   });
 
 });
